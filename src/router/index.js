ad817c288ce126f6f82365af51c1657473d1db40
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2017-10-25 09:53:06 +0200

    router props

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,22 @@ import Showreel from '@/components/Showreel'
 
 Vue.use(Router)
 
+console.log(Router);
+
 export default new Router({
+  //props: true,
   routes: [
     {
-      path: '/',
+      path: '/:path0/',
       name: 'Diwanee Showreel',
-      component: Showreel
+      component: Showreel,
+      props: (route) => ({
+        query: route.query,
+        params: route.params
+      })
     }
   ]
-})
+});
+
+
+      //path: '/:selected.publication/:selected.type/:selected.brand/:selected.category/:selected.subcategory',
